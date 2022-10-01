2ba7d49085852bbcf4164309837bb12e90ee866b
RaviStimphil <97135286+RaviStimphil@users.noreply.github.com>
2022-10-01 13:28:13 -0500

    Update calendar.dart

--- lib/calendar.dart
+++ lib/calendar.dart
@@ -3,41 +3,39 @@ import 'dart:html';
 import 'package:flutter/material.dart';
 import 'package:table_calendar/table_calendar.dart';
 
-class calendar extends StatefulWidget {
-  @Override
-  _CalendarState createState() => _CalendarState();
+class Calendar extends StatefulWidget {
+  @override
+  CalendarState createState() => CalendarState();
 }
 
-class _calenderState extends State<Calendar> {
+class CalendarState extends State<Calendar> {
   CalendarFormat format = CalendarFormat.month;
   DateTime selectedDay = DateTime.now();
   DateTime focusedDay = DateTime.now();
 
-  const Calendar({Key? key, required this.title}) : super(key: key);
-  final String title;
-  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Calendar"),
       ),
       body: TableCalendar(
-          focusedDay: DateTime.now(),
-          firstDay: DateTime(2022, 7),
-          lastDay: DateTime(2023, 1),
-          calenderFormat: format,
-          onFormatChanged: (CalendarFormat _format) {
-            set(() {
-              format = _format;
-            });
-          },
-          daysOfWeekVisible: true, onDaySelected: (DateTime selectDay, DateTime focusDay){
-            setState(() {
-              selectedDay = selectDay;
-              focusedDay = focusDay;
-            });
-          },
-        ),
+        focusedDay: DateTime.now(),
+        firstDay: DateTime(2022, 7),
+        lastDay: DateTime(2023, 1),
+        calendarFormat: format,
+        onFormatChanged: (CalendarFormat _format) {
+          setState(() {
+            format = _format;
+          });
+        },
+        daysOfWeekVisible: true,
+        onDaySelected: (DateTime selectDay, DateTime focusDay) {
+          setState(() {
+            selectedDay = selectDay;
+            focusedDay = focusDay;
+          });
+        },
+      ),
     );
   }
 }
